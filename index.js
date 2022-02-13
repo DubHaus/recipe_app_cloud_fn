93c818447a15b5c5bbff4168cc862ad943f6527a
lebedev_yua <lebedev_yua@proit.ru>
2022-02-13 18:41:31 +0300

    init webhooks

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-exports.Helloworlds = (req, res) => {
-    console.log('Hello world!')
-};
+
+
+// TODO: change entry point to exports.handleWebhook in cloud function
+exports.handleWebhook = (request, response) => {
+
+    const jsonResponse = {
+      fulfillment_response: {
+        messages: [
+          {
+            text: {
+              //fulfillment text response to be sent to the agent
+              text: ["Hi! this is webhook responce"],
+            },
+          },
+        ],
+      },
+    };
+  
+    response.send(jsonResponse);
+  };
+  
